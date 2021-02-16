00df9d944f4602eaf5baf492bc1c5cd5a293ae77
roi.kramer <roi.kramer@codefresh.io>
2021-02-16 17:32:21 +0200

    wip

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ docker run --rm --env URLS=https://g.codefresh.io,https://github.com --env DEBUG
 
 ### Environment Variables:
 - `URLS` - a list of urls seperated by ",".
+- `KUBERNETES_HOST` - kubernetes apiserver address to test connectivity to.
 - `INSECURE` - disable tls certificate validation when using tls.
 - `DEBUG` - can be either '1' or '0', default is '0'.
 - `IN_CLUSTER` - if this is set to '1' errors will be written to `/dev/termination-log`. see [here](https://kubernetes.io/docs/tasks/debug-application-cluster/determine-reason-pod-failure/).
--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+timeout=60 # spend no more than 10s per request
+testResult=0 # store final test result here
+export IFS=","
+
+set -o pipefail
+
 function logDebug() {
     if [[ "$DEBUG" -eq "1" ]]; then
         echo $1
@@ -10,7 +16,6 @@ function log() {
     echo $1
 }
 
-timeout=60 # spend no more than 10s per request
 function testUrl() {
     if [[ "$INSECURE" -eq "1" ]]; then
         curl --max-time $timeout -ksSf $1 > /dev/null 2>err.log
@@ -21,26 +26,67 @@ function testUrl() {
     return $?
 }
 
-export IFS=","
+function testUrls() {
+    for url in $URLS; do
+        logDebug "testing: $url"
+        testUrl $url
+        testExitCode=$?
+        if [[ "$DEBUG" -eq "1" ]]; then
+            cat err.log # print error to logs
+        fi
+        if [[ "$testExitCode" -ne "0" ]]; then
+            if [[ "$IN_CLUSTER" -eq "1" ]]; then
+                cat err.log >> /dev/termination-log # write termination message
+            fi
+            log "can't reach: $url"
+            testResult=1
+        else
+            logDebug "success: $url"
+        fi
+    done
+}
 
-testResult=0
-for url in $URLS; do
-  logDebug "testing: $url"
-  testUrl $url
-  testExitCode=$?
-  if [[ "$DEBUG" -eq "1" ]]; then
-    cat err.log # print error to logs
-  fi
-  if [[ "$testExitCode" -ne "0" ]]; then
-    if [[ "$IN_CLUSTER" -eq "1" ]]; then
-        cat err.log >> /dev/termination-log # write termination message
+function testKubernetes() {
+    if [[ ! -z "$KUBERNETES_HOST" ]]; then
+        logDebug "testing connectivity to kubernetes apiserver at: $KUBERNETES_HOST"
+
+        if [[ "$INSECURE" -eq "1" ]]; then
+            echo "" | kubectl version --server $KUBERNETES_HOST --request-timeout=3s -v=8 --insecure-skip-tls-verify=true > /dev/null 2>err.log
+        else
+            echo "" | kubectl version --server $KUBERNETES_HOST --request-timeout=3s -v=8 > /dev/null 2>err.log
+        fi
+    else
+        logDebug "testing connectivity to kubernetes apiserver"
+        if [[ "$INSECURE" -eq "1" ]]; then
+            echo "" | kubectl version --request-timeout=3s -v=8 --insecure-skip-tls-verify=true > /dev/null 2>err.log
+        else
+            echo "" | kubectl version --request-timeout=3s -v=8 > /dev/null 2>err.log
+        fi
     fi
-    log "can't reach: $url"
-    testResult=1
-  else
-    logDebug "success: $url"
-  fi
-done
+
+    exitStatus=$?
+
+    if [[ "$DEBUG" -eq "1" ]]; then
+        cat err.log # print error to logs
+    fi
+
+    if [[ "$exitStatus" -ne "0" ]]; then
+        if [[ "$IN_CLUSTER" -eq "1" ]]; then
+            cat err.log >> /dev/termination-log # write termination message
+        fi
+        if [[ ! -z "$KUBERNETES_HOST" ]]; then
+            log "can't reach kubernetes apiserver at: $KUBERNETES_HOST. try to specify a different apiserver address."
+        else
+            log "can't reach kubernetes apiserver. try to specify a different apiserver address."
+        fi
+        testResult=1
+    else
+        logDebug "success: kubernetes apiserver"
+    fi
+}
+
+testUrls
+testKubernetes
 
 if [[ $testResult -eq 1 ]]; then
     log "network test failed!"
