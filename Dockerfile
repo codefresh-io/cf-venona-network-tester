5b6d9a0fa00613742b4716af475c50393efd719b
roi.kramer <roi.kramer@codefresh.io>
2021-02-17 14:34:51 +0200

    Revert "Cr 3334 fix (#5)"
    This reverts commit 82222233bf9d79ad4b25f8f44056009f7b2922d4.

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ USER root
 
 # add bash
 RUN apk add --no-cache bash
+RUN curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+RUN chmod +x ./kubectl
+RUN mv ./kubectl /usr/local/bin
 
 WORKDIR /test
 
--- test.sh
+++ test.sh
@@ -47,15 +47,22 @@ function testUrls() {
 }
 
 function testKubernetes() {
-    token=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-    hostAddr=""
     if [[ ! -z "$KUBERNETES_HOST" ]]; then
-        hostAddr="$KUBERNETES_HOST"
+        logDebug "testing connectivity to kubernetes apiserver at: $KUBERNETES_HOST"
+
+        if [[ "$INSECURE" -eq "1" ]]; then
+            echo "" | kubectl version --server $KUBERNETES_HOST --request-timeout=3s -v=8 --insecure-skip-tls-verify=true > /dev/null 2>err.log
+        else
+            echo "" | kubectl version --server $KUBERNETES_HOST --request-timeout=3s -v=8 > /dev/null 2>err.log
+        fi
     else
-        hostAddr="https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT"
+        logDebug "testing connectivity to kubernetes apiserver"
+        if [[ "$INSECURE" -eq "1" ]]; then
+            echo "" | kubectl version --request-timeout=3s -v=8 --insecure-skip-tls-verify=true > /dev/null 2>err.log
+        else
+            echo "" | kubectl version --request-timeout=3s -v=8 > /dev/null 2>err.log
+        fi
     fi
-    logDebug "testing connectivity to kubernetes apiserver at: $hostAddr "
-    curl -sfSk -H "Authorization: Bearer $token" $hostAddr --max-time $timeout > /dev/null 2>err.log
 
     exitStatus=$?
 
@@ -67,7 +74,11 @@ function testKubernetes() {
         if [[ "$IN_CLUSTER" -eq "1" ]]; then
             cat err.log >> /dev/termination-log # write termination message
         fi
-        log "can't reach kubernetes apiserver at: $hostAddr. try to specify a different apiserver address."
+        if [[ ! -z "$KUBERNETES_HOST" ]]; then
+            log "can't reach kubernetes apiserver at: $KUBERNETES_HOST. try to specify a different apiserver address."
+        else
+            log "can't reach kubernetes apiserver. try to specify a different apiserver address."
+        fi
         testResult=1
     else
         logDebug "success: kubernetes apiserver"
