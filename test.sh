6e8215094d00ef5549b615292d66024a92364ea5
roi-codefresh <60569147+roi-codefresh@users.noreply.github.com>
2021-02-17 18:18:40 +0200

    fixed CR-3334 (#9)

--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+timeout=60 # spend no more than 10s per request
+testResult=0 # store final test result here
+export IFS=","
+set -o pipefail
+
 function logDebug() {
     if [[ "$DEBUG" -eq "1" ]]; then
         echo $1
@@ -10,37 +15,68 @@ function log() {
     echo $1
 }
 
-timeout=60 # spend no more than 10s per request
 function testUrl() {
     if [[ "$INSECURE" -eq "1" ]]; then
         curl --max-time $timeout -ksSf $1 > /dev/null 2>err.log
     else
         curl --max-time $timeout -sSf $1 > /dev/null 2>err.log
     fi
-
     return $?
 }
 
-export IFS=","
+function testUrls() {
+    for url in $URLS; do
+        logDebug "testing: $url"
+        testUrl $url
+        testExitCode=$?
+        if [[ "$DEBUG" -eq "1" ]]; then
+            cat err.log # print error to logs
+        fi
+        if [[ "$testExitCode" -ne "0" ]]; then
+            if [[ "$IN_CLUSTER" -eq "1" ]]; then
+                cat err.log >> /dev/termination-log # write termination message
+            fi
+            log "can't reach: $url"
+            testResult=1
+        else
+            logDebug "success: $url"
+        fi
+    done
+}
 
-testResult=0
-for url in $URLS; do
-  logDebug "testing: $url"
-  testUrl $url
-  testExitCode=$?
-  if [[ "$DEBUG" -eq "1" ]]; then
-    cat err.log # print error to logs
-  fi
-  if [[ "$testExitCode" -ne "0" ]]; then
-    if [[ "$IN_CLUSTER" -eq "1" ]]; then
-        cat err.log >> /dev/termination-log # write termination message
+function testKubernetes() {
+    token=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+    hostAddr=""
+    if [[ ! -z "$KUBERNETES_HOST" ]]; then
+        hostAddr="$KUBERNETES_HOST"
+    else
+        hostAddr="https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT"
     fi
-    log "can't reach: $url"
-    testResult=1
-  else
-    logDebug "success: $url"
-  fi
-done
+    logDebug "testing connectivity to kubernetes apiserver at: $hostAddr "
+    curl -sfSk -H "Authorization: Bearer $token" $hostAddr --max-time $timeout > /dev/null 2>err.log
+    exitStatus=$?
+    if [[ "$DEBUG" -eq "1" ]]; then
+        cat err.log # print error to logs
+    fi
+
+    if [[ "$exitStatus" -ne "0" ]]; then
+        if [[ "$exitStatus" -eq "22" ]]; then
+            # exit code 22 means we got 403 Forbidden response, which is good enough.
+            logDebug "success: kubernetes apiserver"
+            return
+        fi
+        if [[ "$IN_CLUSTER" -eq "1" ]]; then
+            cat err.log >> /dev/termination-log # write termination message
+        fi
+        log "can't reach kubernetes apiserver at: $hostAddr. try to specify a different apiserver address."
+        testResult=1
+    else
+        logDebug "success: kubernetes apiserver"
+    fi
+}
+
+testUrls
+testKubernetes
 
 if [[ $testResult -eq 1 ]]; then
     log "network test failed!"
@@ -49,4 +85,3 @@ else
     log "network test finished successfully!"
     exit 0
 fi
-
